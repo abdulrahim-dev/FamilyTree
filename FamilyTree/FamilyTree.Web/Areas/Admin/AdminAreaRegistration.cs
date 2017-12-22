b83229151f511d1b19889b1d06a7ce23b7eb61d7
abdulrahim.ka@luminescent-digital.com <abdulrahim.ka@luminescent-digital.com>
2017-12-22 15:39:09 +0530

    Added Home page in root Views folder. Creates Area for *Admin* and added Routing rules in ***AdminAreaRegistration.cs** file

---
+++ FamilyTree/FamilyTree.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace FamilyTree.Web.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        /// <summary>
+        /// Here, we need to specify the routing inside the area 
+        /// </summary>
+        /// <param name="context"></param>
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_",
+                "admin",
+                new { controller = "Home", action = "Index" },
+                namespaces: new[] { "FamilyTree.Web.Areas.Admin.Controllers" }
+            );
+
+            context.MapRoute(
+                "Admin_default",
+                "admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "FamilyTree.Web.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
---
+++ FamilyTree/FamilyTree.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FamilyTree.Data;
+
+namespace FamilyTree.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Only FOr Initialise the database
+            //DatabaseInitializer sd=new DatabaseInitializer();
+            //sd.Initialize(new FamilyTreeDbContext());
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
