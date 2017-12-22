293efeec0d85bca89452207b3cdd7a7e57afe483
abdulrahim.ka@luminescent-digital.com <abdulrahim.ka@luminescent-digital.com>
2017-12-22 12:23:05 +0530

    Added mapping definitions for automapper

---
+++ FamilyTree/FamilyTree.Web/Mapping/MappingDefinition.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using FamilyTree.Core.Entities.Foundation;
+using FamilyTree.Web.Mapping.References;
+
+namespace FamilyTree.Web.Mapping
+{
+    public class MappingDefinitions
+    {
+        public void Initialise()
+        {
+            _autoRegistrations();
+        }
+
+        private void _autoRegistrations()
+        {
+            var dataEntities =
+                ReferencedAssemblies.Domain.
+                    GetTypes().Where(x => typeof(IEntity).IsAssignableFrom(x)).ToList();
+
+            var dtos =
+                ReferencedAssemblies.Dto.
+                GetTypes().Where(x => x.Name.EndsWith("Dto", true, CultureInfo.InvariantCulture)).ToList();
+
+            foreach (var entity in dataEntities)
+            {
+                //if (Mapper.Configuration.GetAllTypeMaps().FirstOrDefault(m => m.DestinationType == entity || m.SourceType == entity) == null)
+                //{
+                var matchingDto =
+                    dtos.FirstOrDefault(x => x.Name.Replace("Dto", string.Empty).Equals(entity.Name, StringComparison.InvariantCultureIgnoreCase));
+
+                if (matchingDto != null)
+                {
+                    Mapper.Initialize(cfg => {
+                        cfg.CreateMap(entity, matchingDto);
+                    });
+                    Mapper.Initialize(cfg => {
+                        cfg.CreateMap(matchingDto, entity);
+                    });
+                }
+                // }
+            }
+
+        }
+
+    }
+}
---
+++ FamilyTree/FamilyTree.Web/Mapping/References/ReferencedAssemblies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace FamilyTree.Web.Mapping.References
+{
+    public static class ReferencedAssemblies
+    {
+        public static Assembly Services
+        {
+            get { return Assembly.Load("FamilyTree.Services"); }
+        }
+
+        public static Assembly Repositories
+        {
+            get { return Assembly.Load("FamilyTree.Data"); }
+        }
+
+        public static Assembly Dto
+        {
+            get
+            {
+                return Assembly.Load("FamilyTree.Dto");
+            }
+        }
+
+        public static Assembly Domain
+        {
+            get
+            {
+                return Assembly.Load("FamilyTree.Core");
+            }
+        }
+    }
+}
