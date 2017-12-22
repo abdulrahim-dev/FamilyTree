847236929d7eff0b4f2267396bf5d9dd36610b55
abdulrahim.ka@luminescent-digital.com <abdulrahim.ka@luminescent-digital.com>
2017-12-22 10:03:20 +0530

    Added Microsoft.AspNet.Identity.EntityFramework to *Core* project. Also Created  required interfaces for future development

---
+++ FamilyTree/FamilyTree.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+
+namespace FamilyTree.Core.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+
+            return userIdentity;
+        }
+
+        public ApplicationUser()
+        {
+            DateCreated = DateTime.Now;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime? LastLoginDate { get; set; }
+
+        public bool Activated { get; set; }
+
+    }
+}
