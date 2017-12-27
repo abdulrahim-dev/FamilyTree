a533045372421a73c1b22c4b85686bdf4cd750f2
abdulrahim.ka@luminescent-digital.com <abdulrahim.ka@luminescent-digital.com>
2017-12-27 10:29:49 +0530

    OnAuthorization overriding for Authorize admin

--- FamilyTree/FamilyTree.Web/Attributes/AdminAuthorizeAttribute.cs
+++ FamilyTree/FamilyTree.Web/Attributes/AdminAuthorizeAttribute.cs
@@ -1,38 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace FamilyTree.Web.Attributes
 {
     public class AdminAuthorizeAttribute : AuthorizeAttribute
     {
-        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
-        {
 
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
             var context = filterContext.HttpContext;
             if (context.User != null && context.User.Identity != null)
             {
                 if (context.User.Identity.IsAuthenticated)
                 {
-                    if (context.User.IsInRole("Admin"))
+                    if (!context.User.IsInRole("Admin"))
                     {
-                        filterContext.Result = new RedirectToRouteResult(new
-                       RouteValueDictionary(new { controller = "Home", action = "Index" }));
+                        filterContext.Result = new RedirectResult("/");
                     }
                 }
                 else
                 {
                     filterContext.Result = new RedirectResult("/login");
                 }
-                
+
             }
-            //else
-            //{
-            //    base.HandleUnauthorizedRequest(filterContext);
-            //}
         }
         //
     }
