894736d43fa2794ca5279f87bfc8b6a53e4347f8
abdulrahim.ka@luminescent-digital.com <abdulrahim.ka@luminescent-digital.com>
2017-12-28 10:54:44 +0530

    Created ICategoryService in Core-->Service and implemented it in Services-->Services folder

---
+++ FamilyTree/FamilyTree.Core/Services/ICategoryService.cs
@@ -0,0 +1,8 @@
+﻿using FamilyTree.Core.Entities;
+
+namespace FamilyTree.Core.Services
+{
+    public interface ICategoryService:IService<Category>
+    {
+    }
+}
---
+++ FamilyTree/FamilyTree.Services/Services/CategoryService.cs
@@ -0,0 +1,14 @@
+﻿using FamilyTree.Core.Data;
+using FamilyTree.Core.Entities;
+using FamilyTree.Core.Services;
+
+namespace FamilyTree.Services.Services
+{
+    public class CategoryService:BaseService<Category>,ICategoryService
+    {
+        public CategoryService(IUnitOfWork unitOfWork) :base(unitOfWork)
+        {
+
+        }
+    }
+}
