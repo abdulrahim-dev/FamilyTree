49500a4547cb507890587ad267462d4176421860
abdulrahim.ka@luminescent-digital.com <abdulrahim.ka@luminescent-digital.com>
2017-12-28 10:48:43 +0530

    Removed WebApi controller Register for Now, We can implement it later

--- FamilyTree/FamilyTree.Web/DI_Capsule/Modules/ControllerCapsuleModule.cs
+++ FamilyTree/FamilyTree.Web/DI_Capsule/Modules/ControllerCapsuleModule.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web;
+﻿using System.Reflection;
 using Autofac;
 using Autofac.Integration.Mvc;
-using Autofac.Integration.WebApi;
 
 namespace FamilyTree.Web.DI_Capsule.Modules
 {
@@ -15,11 +10,12 @@ namespace FamilyTree.Web.DI_Capsule.Modules
         {
 
             // Register the MVC Controllers
+           
             builder.RegisterControllers(Assembly.Load("FamilyTree.Web"));
 
             // Register the Web API Controllers
             //builder.RegisterApiControllers(Assembly.GetCallingAssembly());
-            builder.RegisterApiControllers(Assembly.Load("FamilyTree.Web"));
+            //builder.RegisterApiControllers(Assembly.Load("FamilyTree.Web"));
 
         }
     }
